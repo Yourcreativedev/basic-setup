37abce8c16629830aacf4a4db072c403544c853c
Yourcreativedev <shyamendrahazra@gmail.com>
2022-08-30 19:24:13 +0530

    Added! github documentation link in the git-setup file

--- git-setup.sh
+++ git-setup.sh
@@ -2,3 +2,5 @@
 read -p "Enter Your Mail : " email
 ssh-keygen -t ed25519 -C $email
 cat ~/.ssh/id_ed25519.pub
+
+echo "refer to this link : https://docs.github.com/en/authentication/connecting-to-github-with-ssh/adding-a-new-ssh-key-to-your-github-account"
